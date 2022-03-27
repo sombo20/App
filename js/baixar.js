d1ec2420c4e969167adaf4c999fff108e43978c7
VICENTE <64757262+sombo20@users.noreply.github.com>
2022-03-27 12:19:18 -0300

    Add files via upload

--- js/baixar.js
+++ js/baixar.js
@@ -1,16 +1,10 @@
-
- 
-	if(localStorage){
-		if(localStorage['baixa']==undefined){
-			localStorage['baixa']=0;
+yu=function(id){return document.getElementById(id);}
+if(localStorage){
+if(localStorage['visitas']==undefined){
+localStorage['visitas']=0;
+}var n=parseInt(localStorage['visitas']);
+localStorage['visitas']=1+n;
+var num=localStorage['visitas'];
+var mensaje=  num ;
+yu('visitas').innerHTML=mensaje;
 }
-	var n=parseInt(localStorage['baixa']);localStorage['baixa']=1+n;
-	var num=localStorage['baixa'];
-
-var mensagens= num ;
-document.getElementById("visitas").innerHTML = mensagens;
-	}
-
-
-
-
